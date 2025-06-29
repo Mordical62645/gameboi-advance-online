34b83eb339abd9075bc265754e4ce61f2373ad55
Mordical62645 <p.tecson.rizal@gmail.com>
2025-06-29 21:28:50 +0800

    basic ram simulation fuckery

--- ramsim.c
+++ ramsim.c
@@ -6,18 +6,34 @@
 
 # define RAM_SIZE 256 // 256 bytes of ram
 
-uint8_t ram[RAM_SIZE]; // RAM as byte array
+// uint8_t      = 8 bits    = 1 byte unsigned integer   = values 0 - 255
+// uint16_t     = 16 bits   = 2 byte unsigned integer.  = values 0 - 65535 (64KB RAM    )
+
+
+uint8_t ram[RAM_SIZE];  // RAM as byte array
 
 // Read a byte from RAM
-uint8_t read_byte(uint8_t addr) {
+uint8_t read_byte(uint16_t addr) {
     if (addr >= RAM_SIZE) {
         printf("Error: Address out of bounds\n");
         return 0;
     }
-    return ram[addr];
+    return ram[addr]; // returns a value at addr
 }
 
+// Write a byte to RAM
+void write_byte(uint16_t addr, uint8_t value) {
+    if (addr >= RAM_SIZE){
+        printf("Error: Address out of bounds\n");
+        return;
+    }
+    ram[addr] = value;
+}
+
+// Test
 int main(){
-    printf("test\n");
+    write_byte(0x10, 0xAB); // Write 0xAB to address 0x10
+    printf("Read 0x%X from address 0x10", read_byte(0x10));
+
     return 0;   
 }
