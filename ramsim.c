892b0588c1b1d4727fe4c40bc43f125c073a1093
Mordical62645 <p.tecson.rizal@gmail.com>
2025-06-29 21:07:09 +0800

    RAM Simulator reading a byte from RAM created

--- ramsim.c
+++ ramsim.c
@@ -2,6 +2,20 @@
 // Write a basic command-line calculator using bitwise ops
 
 # include <stdio.h>
+# include <stdint.h>
+
+# define RAM_SIZE 256 // 256 bytes of ram
+
+uint8_t ram[RAM_SIZE]; // RAM as byte array
+
+// Read a byte from RAM
+uint8_t read_byte(uint8_t addr) {
+    if (addr >= RAM_SIZE) {
+        printf("Error: Address out of bounds\n");
+        return 0;
+    }
+    return ram[addr];
+}
 
 int main(){
     printf("test\n");
