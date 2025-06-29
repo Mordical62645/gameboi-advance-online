52e9621b7f5bd8cacbc4b9b11b4e23183ff8498c
Mordical62645 <p.tecson.rizal@gmail.com>
2025-06-29 14:43:25 +0800

    Core emulator

---
+++ ramsim.c
@@ -0,0 +1,10 @@
+// Create a RAM simulator: read_byte(addr) and write_byte(addr, value)
+// Write a basic command-line calculator using bitwise ops
+
+# include <stdio.h>
+
+int main(){
+    printf("test\n");
+    return 0;
+    
+}
